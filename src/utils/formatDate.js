213aff915a62cef2ae84d62a309e30089578d1e7
tiendev2003 <tientc.21it@vku.udn.vn>
2025-02-02 20:22:09 +0700

    update

--- src/utils/formatDate.js
+++
@@ -1,6 +0,0 @@
-const formatDate = (dateString) => {
-  const options = { year: 'numeric', month: 'long', day: 'numeric' , hour: 'numeric', minute: 'numeric', second: 'numeric' };
-    return new Date(dateString).toLocaleDateString(undefined, options);
-};
-
-export default formatDate;
--- src/utils/formatMoney.js
+++
@@ -1,8 +0,0 @@
-const formatCurrency = (value) => {
-    return new Intl.NumberFormat("vi-VN", {
-      style: "currency",
-      currency: "VND",
-    }).format(value);
-  };
-
-export default formatCurrency;
